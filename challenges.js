7a4032a5459b370b730614d9fc652aaa2c3be504
Ahmed Elafifi <elafifi2015@gmail.com>
2020-04-22 05:51:58 +0200

    v3 of my game after solving challenge 2

--- challenges.js
+++ challenges.js
@@ -8,16 +8,6 @@ Change the game to follow these rules:
 */
 
 
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 
 /*
 GAME RULES:
@@ -83,10 +73,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // 1. Add currentScore to globalScroe 
         scores[activePlayer] += roundScore;
 
+        var inputScore = document.querySelector('.final_score').value;
+        var winningScore;
+        // undefined, null, 0 or "" ==> false 
+        // other = true;
+        if(inputScore && !isNaN(inputScore)) {
+            winningScore = inputScore;
+        } else {
+            winningScore = 100;
+        }
         // 2. Update UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
         // Check winner 
-        if(scores[activePlayer] >= 10) {
+        if(scores[activePlayer] >= inputScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -102,6 +101,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
 
 });
+
 
 document.querySelector('.btn-new').addEventListener('click', initGame);
 
@@ -144,5 +144,6 @@ function initGame() {
     document.querySelector('.player-1-panel').classList.remove('active');
 
     document.querySelector('.player-0-panel').classList.add('active');
+
 
 }
